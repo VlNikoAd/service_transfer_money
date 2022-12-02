ef89fd95b9b5a6b1a528ce0aaa0d8429745e311c
VlNikoAd <nikolaenko_vladislav@icloud.com>
2022-12-02 15:31:50 +0300

    created basic project

---
+++ src/main/java/ru/netology/service_transfer_money/ServiceTransferMoneyApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.service_transfer_money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTransferMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTransferMoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/service_transfer_money/ServiceTransferMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.service_transfer_money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTransferMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
